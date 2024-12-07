f2828867087d5be3ca16da1ed9ef6fb2e71b3591
Chetali Goyal <goyalchetali123@gmail.com>
2024-12-07 15:55:01 +0530

    completed the static common components

--- dashboard-frontend/src/app/(dashboard)/layout.tsx
+++ dashboard-frontend/src/app/(dashboard)/layout.tsx
@@ -19,7 +19,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="h-screen flex">
+    <section className="min-h-screen flex">
       {/* LEFT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
@@ -32,7 +32,7 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-auto">
         <Navbar />
         {children}
       </div>
