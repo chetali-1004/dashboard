75dd08351013bc7b1500760f29a56ede14454bb3
Chetali Goyal <goyalchetali123@gmail.com>
2024-12-08 13:35:08 +0530

    created announcements section for admin page

---
+++ dashboard-frontend/src/components/Announcements.tsx
@@ -0,0 +1,50 @@
+const Announcements = () => {
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Announcement</h1>
+        <span className="text-xs text-gray-400">View All</span>
+      </div>
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="bg-lamaSkyLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm mt-1 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            esse, suscipit eligendi odio a porro error doloribus vel quasi
+          </p>
+        </div>
+        <div className="bg-lamaPurpleLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm mt-1 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            esse, suscipit eligendi odio a porro error doloribus vel quasi
+          </p>
+        </div>
+        <div className="bg-lamaYellowLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm mt-1 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            esse, suscipit eligendi odio a porro error doloribus vel quasi
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Announcements;
